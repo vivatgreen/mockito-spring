b2a54de551e56bc18ddf3741b7a49811fef15c56
vivatgreen <grindsasha@bk.ru>
2022-06-13 14:06:49 +0300

    Initial commit

---
+++ src/test/java/pro/sky/mockito/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mockito.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
